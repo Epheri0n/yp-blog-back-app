9b96dd953814c67c1ba3a36c38ffaf8817035ec4
Egor <gopot-09@bk.ru>
2026-09-21 22:46:43 +0300

    add blog service

--- src/main/java/demo/blog/model/SearchFilter.java
+++ src/main/java/demo/blog/model/SearchFilter.java
@@ -1,8 +1,30 @@
 package demo.blog.model;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public record SearchFilter(
         String title,
-        List<String> tags) {
+        List<String> tags)
+{
+    public SearchFilter {
+        tags = List.copyOf(tags);
+    }
+
+    public static SearchFilter parse(String search) {
+        List<String> words = new ArrayList<>();
+        var tags = new LinkedHashSet<String>();
+        for (String word : search.split("\\s+")) {
+            if (word.isBlank()) {
+                continue;
+            }
+            if (word.startsWith("#")) {
+                tags.add(word.substring(1));
+            } else {
+                words.add(word);
+            }
+        }
+        return new SearchFilter(String.join(" ", words), List.copyOf(tags));
+    }
 }
